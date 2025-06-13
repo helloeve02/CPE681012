694e5e67595febd5ca1e0d5d67e6233a16f884e1
Supaluck Tohthong <spltt.38252@gmail.com>
2025-06-13 13:25:14 +0700

    add

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,97 @@
 package main
+
+
+import (
+
+   "net/http"
+
+
+   "github.com/gin-gonic/gin"
+
+
+//    "github.com/helloeve02/CPE681012/config"
+
+
+   "github.com/helloeve02/CPE681012/middlewares"
+
+)
+
+
+const PORT = "8000"
+
+
+func main() {
+
+
+   // open connection database
+
+//    config.ConnectionDB() //ยังไม่ได้เขียน db
+
+
+   // Generate databases
+
+//    config.SetupDatabase() //ยังไม่ได้เขียน db
+
+
+   r := gin.Default()
+
+
+   r.Use(CORSMiddleware())
+
+
+   // Auth Route
+
+   router := r.Group("/")
+
+   {
+
+       router.Use(middlewares.Authorizes())
+
+
+   }
+
+
+
+   r.GET("/", func(c *gin.Context) {
+
+       c.String(http.StatusOK, "API RUNNING... PORT: %s", PORT)
+
+   })
+
+
+   // Run the server
+
+
+   r.Run("localhost:" + PORT)
+
+
+}
+
+
+func CORSMiddleware() gin.HandlerFunc {
+
+   return func(c *gin.Context) {
+
+       c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+       c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+       c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+
+       c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+
+       if c.Request.Method == "OPTIONS" {
+
+           c.AbortWithStatus(204)
+
+           return
+
+       }
+
+
+       c.Next()
+
+   }
+
+}
